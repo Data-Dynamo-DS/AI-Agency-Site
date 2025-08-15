97fc9c5750396286fe2a63005ceeb7bbd27fcdbc
Sam C <data_dynamo.work@proton.me>
2025-08-16 02:25:45 +0530

    Rebuild with basePath and export to docs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/AI-Agency-Site',
+  distDir: 'docs',
+  basePath: '/AI-Agency-Site', // match your GitHub repo name here exactly
   images: {
     domains: ['localhost'],
   },
-}
+};
 
 module.exports = nextConfig;
